1d1a3d5a06594bc57a116dd641e054159fb0643c
Lothron <yuewuxd@gmail.com>
2022-08-15 15:35:58 +0800

    -

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,8 @@
-//============================================
-//       Language options dropdown list
-//============================================
+//========================================================
+//           Language options dropdown list
+//      modified based on w3schools sample code 
+// https://www.w3schools.com/howto/howto_js_dropdown.asp
+//========================================================
 // When the language icon is clicked, toggle between hiding and showing the language options
 function openLanguageOptions() {
   document.getElementById("languages").classList.toggle("show");
@@ -15,3 +17,4 @@ window.onclick = function (e) {
     }
   }
 };
+
