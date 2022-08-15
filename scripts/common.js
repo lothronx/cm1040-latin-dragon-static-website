ffd247799dc466b24816c5d9333a2c5a75a7d3d0
Lothron <yuewuxd@gmail.com>
2022-08-15 17:13:14 +0800

    -

--- scripts/common.js
+++ scripts/common.js
@@ -1,20 +1,21 @@
 //========================================================
 //           Language options dropdown list
-//      modified based on w3schools sample code 
-// https://www.w3schools.com/howto/howto_js_dropdown.asp
 //========================================================
+let languagesBtn = document.getElementById("language-btn");
+let languages = document.getElementById("languages");
+
 // When the language icon is clicked, toggle between hiding and showing the language options
+languagesBtn.addEventListener("click", openLanguageOptions);
 function openLanguageOptions() {
-  document.getElementById("languages").classList.toggle("show");
+  languages.classList.toggle("show");
 }
 
 //Hide the language options when clicked elsewhere
-window.onclick = function (e) {
-  if (!e.target.matches(".fa-language")) {
-    let languages = document.getElementById("languages");
-    if (languages.classList.contains("show")) {
-      languages.classList.remove("show");
-    }
+window.addEventListener("click", closeLanguageOptions);
+function closeLanguageOptions(e) {
+  if (
+    !e.target.matches(".fa-language") & languages.classList.contains("show")
+  ) {
+    languages.classList.remove("show");
   }
-};
-
+}
