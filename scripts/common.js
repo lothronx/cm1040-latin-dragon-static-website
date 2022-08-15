be205b1601e6cff525cd74fe60267d62d3287386
Lothron <yuewuxd@gmail.com>
2022-08-15 19:21:55 +0800

    -

--- scripts/common.js
+++ scripts/common.js
@@ -20,20 +20,20 @@ function closeLanguageOptions(e) {
 
 //========================================================
 //               Homepage sidebar items
-//   Click the sidebar item to jump to its location
+//   Click the sidebar item to jump to its section
 //========================================================
-document.getElementById("index-home").addEventListener("click", function () {
+document.getElementById("index-home-btn").addEventListener("click", function () {
   window.location.href = "index.html#index-home";
 });
-document.getElementById("index-whitepaper").addEventListener("click", function () {
+document.getElementById("index-whitepaper-btn").addEventListener("click", function () {
   window.location.href = "index.html#index-whitepaper";
 });
-document.getElementById("index-news").addEventListener("click", function () {
+document.getElementById("index-news-btn").addEventListener("click", function () {
   window.location.href = "index.html#index-news";
 });
-document.getElementById("index-faq").addEventListener("click", function () {
+document.getElementById("index-faq-btn").addEventListener("click", function () {
   window.location.href = "index.html#index-faq";
 });
-document.getElementById("index-contact").addEventListener("click", function () {
+document.getElementById("index-contact-btn").addEventListener("click", function () {
   window.location.href = "index.html#index-contact";
 });
